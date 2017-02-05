15ca9d2aa38ca12bec96d142b81cfb565c2b5bfa
overgrownasian <branden@overgrownasian.com>
2017-02-05 02:22:38 -0700

    QRL Launcher

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "cordova-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "cordova-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-barcodescanner": "0.7.0",
+    "cordova-plugin-whitelist": "1.3.1"
+}
+// BOTTOM OF METADATA
+});
